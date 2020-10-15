1636be76bd7b8e5befe0312f0940bf60c5c61d96
z1786542383 <72176403+z17865422383@users.noreply.github.com>
2020-10-15 17:51:20 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('welcome|come|here|to start|#circle|#countdown 3|李壮壮');
+      S.UI.simulate('welcome|come|here|to start|#circle|#countdown 3|enjoy yourself！');
     }
 
     S.Drawing.loop(function () {
